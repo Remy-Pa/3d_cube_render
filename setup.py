fafb343ae65406fd5a8c69ebdafa442fc693f943
Remy Paquette <remy.paquette@gmail.com>
2025-01-10 22:21:29 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='3d_cube_render',
+    version='0.1.0',
+    author='Remy-Pa',
+    author_email='remy.paquette@gmail.com',
+    description='Adds an App class which takes in lists of coordinates and generates a window rendering cubes at the specified coordinates using OpenGL and pygame',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Remy-Pa/3d_cube_render/',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'pygame',
+        'PyOpenGL',
+        'PyOpenGL-accelerate',
+        'numpy'        
+    ],
+)
