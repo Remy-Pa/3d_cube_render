2fb7caeab97342eea03d5d707e983452b3b96a8b
Remy Paquette <remy.paquette@gmail.com>
2025-01-10 22:30:37 -0500

    Update __init__.py

--- cube_3d_render/__init__.py
+++ cube_3d_render/__init__.py
@@ -1,4 +1,4 @@
-from 3d_cube_render.3d_cube_render import App
-from 3d_cube_render.cube import Cube
+from cube_3d_render.cube_3d_render import App
+from cube_3d_render.cube import Cube
 
 __all__ = ['3d_cube_render', 'cube']
